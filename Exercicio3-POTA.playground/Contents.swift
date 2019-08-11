f05d1ed979f2ae51e9da59743847bd142f49873e
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-08-11 14:44:00 -0400

    Adiciona Exercício3

---
+++ Exercicio3-POTA.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+//Construa um algoritmo que leia dois números, o primeiro maior que o segundo, e os coloque em ordem crescente.
+
+func sort(num: Array<Int>){
+    
+    print(num.sorted())
+    
+}
+
+sort(num: [9,8,7,6,5,4,3,2,1])
