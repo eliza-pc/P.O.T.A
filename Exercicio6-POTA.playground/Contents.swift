1e85510eaf428954bba5116efeb64c6df2d8e948
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-08-11 15:08:30 -0400

    Adiciona Exercício6

---
+++ Exercicio6-POTA.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+//Faça um algoritmo que ordene 5 números inteiros em um vetor.
+
+func sort(num: Array<Int>){
+        
+    print(num.sorted())
+        
+}
+    
+sort(num: [9,8,7,6,5])
+    
+
+
